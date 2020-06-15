e4f5d324e27d0572a40ca9e62a57135c340ef2a6
kfatkin <kfatkin@gmail.com>
2020-06-15 17:26:33 -0400

    Add in DynamoDB table writing and DLQ management

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,9 @@ async function main(sqsObject: AWS.SQS, dynamoObject: AWS.DynamoDB) {
     if (sqsReceiveResponse?.Messages) {
       console.log(sqsReceiveResponse.Messages);
       for (let message of sqsReceiveResponse.Messages) {
-        const dynamoParams = {
+        // console.log(message.Body);
+        let jsonMessageBody = JSON.parse(message.Body);
+        let dynamoParams = {
           Item: {
             messageid: {
               S: message.MessageId,
@@ -27,8 +29,39 @@ async function main(sqsObject: AWS.SQS, dynamoObject: AWS.DynamoDB) {
               S: message.Body,
             },
           },
-          TableName: 'BSS-DevOps-DynamoDB-SES-Notifications',
+          TableName: 'BSS-DevOps-SES-Notifications',
         };
+        console.log(dynamoParams);
+        if (jsonMessageBody.notificationType === 'Bounce') {
+          let messageBodyNotifyType = jsonMessageBody.notificationType;
+          let destinationAddress =
+            jsonMessageBody.bounce.bouncedRecipients[0].emailAddress;
+          let sourceAddress = jsonMessageBody.mail.source;
+          let commonHeaders = jsonMessageBody.mail.commonHeaders;
+          console.log(messageBodyNotifyType, destinationAddress, sourceAddress);
+          Object.assign(dynamoParams.Item, {
+            notificationType: {
+              S: messageBodyNotifyType,
+            },
+          });
+          Object.assign(dynamoParams.Item, {
+            sourceAddress: {
+              S: sourceAddress,
+            },
+          });
+          Object.assign(dynamoParams.Item, {
+            commonHeaders: {
+              M: commonHeaders,
+            },
+          });
+          Object.assign(dynamoParams.Item, {
+            destinationAddress: {
+              S: destinationAddress,
+            },
+          });
+          console.log(dynamoParams);
+        }
+        console.log(dynamoParams);
         try {
           const dynamoResponse = await dynamoObject
             .putItem(dynamoParams)
@@ -57,6 +90,31 @@ async function main(sqsObject: AWS.SQS, dynamoObject: AWS.DynamoDB) {
     }
   } catch (err) {
     console.log(err);
+  }
+
+  const sqsDlqParams = {
+    QueueUrl:
+      'https://sqs.us-east-1.amazonaws.com/880392359248/BSS-DevOps-SES-Notifications-DLQ',
+    MaxNumberOfMessages: 10,
+    VisibilityTimeout: 10,
+    WaitTimeSeconds: 15,
+  };
+  const sqsDlqReceiveResponse = await sqsObject
+    .receiveMessage(sqsParams)
+    .promise();
+  console.log(sqsDlqReceiveResponse);
+  if (sqsDlqReceiveResponse?.Messages) {
+    for (let dlqMessage in sqsDlqReceiveResponse.Messages) {
+      console.log(dlqMessage);
+    }
+  } else {
+    const dlqPurge = await sqsObject
+      .purgeQueue({
+        QueueUrl:
+          'https://sqs.us-east-1.amazonaws.com/880392359248/BSS-DevOps-SES-Notifications-DLQ',
+      })
+      .promise();
+    console.log(dlqPurge);
   }
 }
 
